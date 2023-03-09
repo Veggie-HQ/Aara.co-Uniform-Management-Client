b3b650d4b02ecee828dce37691e3fba980f14034
briha <hello@briha.live>
2023-03-10 02:48:01 +0530

    Navbar and cart
    Co-Authored-By: Anusha Chaturvedi <68815162+anusha-c18@users.noreply.github.com>

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,41 @@
-import { Flex } from "@chakra-ui/react";
-import React from "react";
+import Link from "next/link";
+import { FiShoppingBag, FiUser } from "react-icons/fi";
+import { NavItems } from "@/styles/NavStyles";
+// import Cart from "./Cart";
+import { useStateContext } from "@/lib/context";
+import { Box, Flex, Icon, Image } from "@chakra-ui/react";
+import Cart from "../Cart";
+const { AnimatePresence, motion } = require("framer-motion");
 
-const Index = () => {
-  return <Flex height="50px">This is Nav</Flex>;
-};
+export default function Nav() {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-export default Index;
+  return (
+    <Flex height="80px" bg="white" align="center" justify="space-evenly">
+      <Flex direction="column" align="center">
+        <FiUser />
+        <h3>Profile</h3>
+      </Flex>
+
+      <Box>
+        <Link href="/">
+          <Image src="/assets/logo.jpg" alt="Aara.co Logo" width="100px" />
+        </Link>
+      </Box>
+
+      <Flex direction="column" align="center">
+        <div onClick={() => setShowCart(true)}>
+          {totalQuantities > 0 && (
+            <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
+              {totalQuantities}
+            </motion.span>
+          )}
+          <FiShoppingBag />
+        </div>
+
+        <h3>Cart</h3>
+      </Flex>
+      <AnimatePresence>{showCart && <Cart />}</AnimatePresence>
+    </Flex>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,15 @@
+import { theme } from "@/chakra/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import Nav from "@/components/Nav";
+import { StateContext } from "@/lib/context";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StateContext>
+      <ChakraProvider theme={theme}>
+        <Nav />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </StateContext>
+  );
 }
